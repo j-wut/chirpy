3dd9a4e671a6c2e9f4171ede4c159372087c8243
j-wut <15221514+j-wut@users.noreply.github.com>
2025-04-06 19:43:32 -0400

    add healthz

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"io"
 	"fmt"
 	"net/http"
 )
+
+func readiness(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	io.WriteString(w, "OK")
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -11,7 +18,8 @@ func main() {
 		Addr: ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir("./site")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./site"))))
+	mux.HandleFunc("/healthz", readiness)
 
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println(err) 
