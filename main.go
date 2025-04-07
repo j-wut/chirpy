1c0a8cbfd005db7ff31e93ce804f21789674bbb8
j-wut <15221514+j-wut@users.noreply.github.com>
2025-04-07 15:33:22 -0400

    added middle ware

--- main.go
+++ main.go
@@ -4,7 +4,34 @@ import (
 	"io"
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiState struct {
+	fileserverHits atomic.Int32
+}
+
+func (state *apiState) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		state.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (state *apiState) hitsHandler(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	io.WriteString(w, fmt.Sprintf("Hits: %d", state.fileserverHits.Load()))
+}
+
+func (state *apiState) resetHitsHandler(w http.ResponseWriter, request *http.Request) {
+	state.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	io.WriteString(w, "OK")
+
+}
+
 
 func readiness(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -13,13 +40,23 @@ func readiness(w http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
+
+	metrics := &apiState{
+		fileserverHits: atomic.Int32{},
+	}
+
+	metrics.fileserverHits.Store(0)
+
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./site"))))
+	mux.Handle("/app/", http.StripPrefix("/app", metrics.middlewareMetricsInc(http.FileServer(http.Dir("./site")))))
 	mux.HandleFunc("/healthz", readiness)
+
+	mux.HandleFunc("/metrics", metrics.hitsHandler)
+	mux.HandleFunc("/reset", metrics.resetHitsHandler)
 
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println(err) 
